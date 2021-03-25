ea6a565021eb17630c5f3490f7c20c311649a480
Renata Felix <ava253@einstein.br>
2021-03-25 16:58:53 -0300

    Projeto validador de senhas c/ testes

---
+++ ChallengePasswordItau/Helpers/ReturnHelper.cs
@@ -0,0 +1,37 @@
+﻿using ChallengePasswordItau.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengePasswordItau.Helpers
+{
+    public static class ReturnHelper
+    {
+        public static JsonResult Try(bool? success, string message)
+        {
+            ReturnModel returnModel = new ReturnModel();
+            AnswersModel Answers = new AnswersModel();            
+
+            if (success == null)
+            {
+                Answers = new AnswersModel()
+                {
+                    Message = message,
+                    Status = 0
+                };
+                
+                return new JsonResult(Answers);                               
+            }
+            else
+            {
+                returnModel.returnResult = success;
+
+                return new JsonResult(returnModel);
+            }
+            
+        }
+
+    }    
+}
---
+++ ChallengePasswordItau/Models/ReturnModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengePasswordItau.Models
+{
+    public class ReturnModel
+    {
+        public bool? returnResult { get; set; }                        
+    }
+}
